789d0adaac2d68621aa6bd82418aa9f6450e79d7
leandro <leandrocosta2065@gmail.com>
2025-10-22 19:42:40 +0000

    atualizaçãoooooo

--- src/home/home.js
+++ src/home/home.js
@@ -2,30 +2,35 @@ const movieName = document.getElementById('barra-pesquisa');
 const btnGo = document.getElementById('pesquisar')
 const content = document.getElementById('content');
 const image = document.getElementById('img');
+const limite = 21
+const itensHome = document.getElementById('itens-home');
 
 const fetchApi = (value) => {
-    const result = fetch (`https://api.imdbapi.dev/search/titles?query=${encodeURIComponent(value)}`)
-    .then((res)=> res.json())
-    .then((data)=>{
-        console.log(data);
-        return data;
-    });
-    
+    const result = fetch (`https://api.imdbapi.dev/search/titles?query=${encodeURIComponent(value)}&limit=${limite}`)
+    .then((res)=> {
+        return res;
+    })
     return result;
 }
-
 btnGo.addEventListener('click', async (event) => {
-    
     event.preventDefault();
     const result = await fetchApi(movieName.value);
-    
-    result.titles.forEach(movie => {
-    if (movie.type === "movie" && movie.primaryImage?.url) {
-        const img = document.createElement('img');
-        img.src = movie.primaryImage.url;
-        img.alt = movie.primaryTitle;
-        content.appendChild(img);
-        }
-    });
 
+    if (result.ok){
+        itensHome.style.display = 'none';
+        result.json().then((obras) => {
+            let filmes = obras.titles.filter((titulo) => titulo.type === 'movie' && titulo.primaryImage?.url)
+            let filmes_element = filmes.map((filme) => {
+                let elemento = document.createElement('img')
+                elemento.src = filme.primaryImage.url
+                elemento.alt = filme.primaryTitle
+                return elemento
+            })
+            content.innerHTML = ''
+            filmes_element.forEach(elemento => {
+                content.append(elemento)
+            });
+        });
+    }
 });
+
